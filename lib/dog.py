f508c32c2f405b62dc80a651cc9930f1e20eb2c6
Harriet Chepngeno <harrietseroney2023@gmail.com>
2025-06-15 23:32:15 +0300

    learn both init method and self key words

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    def __init__(self,name, breed="Mutt"):
+        self.name = name
+        self.breed = breed
+
+dog1 = Dog("snoopy", "german-shepherd")
+print(dog1.name)
+print(dog1.breed)
